146f8db34ea333921f8a28ed6e3dd9456695ddbe
Shawn Benny <sbenn9210@gmail.com>
2021-07-05 21:17:51 -0500

    Add small change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>This is new</p>
+        <p>This is new hello world</p>
         <a
           className="App-link"
           href="https://reactjs.org"
